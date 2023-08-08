a2d356f7fe5e82e4a53a8e396485f0a8fc9743dc
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2023-08-08 14:56:34 +0930

    Added error handlers

--- even.c
+++ even.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
+void ErrorHandler(int signalNum){
+    if (signalNum == SIGHUP){
+        printf("Ouch!\n");
+    } else if (signalNum == SIGINT){
+        printf("Yeah!\n");
+    }
+}
+
+// Takes a number n in the command line and prints out
+// the first n even numbers
 int main(int argc, char const *argv[]){
+    signal(SIGINT, ErrorHandler);
+    signal(SIGHUP, ErrorHandler);
+
     int n = 0;
     if (argc == 2){
-        n = atoi(argv[1]) * 2;
+        n = atoi(argv[1]);
     }
     
-    for (int i = 2; i <= n; i+=2){
+    for (int i = 0; i < n; i+=2){
         printf("%d\n", i);
         sleep(5);
     }
