6a9d82a7946a3ec51613cf559b984907f15e22f7
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2023-08-08 14:13:32 +0930

    Added sleep()

--- even.c
+++ even.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[]){
     int n = 0;
@@ -9,6 +10,7 @@ int main(int argc, char const *argv[]){
     
     for (int i = 2; i <= n; i+=2){
         printf("%d\n", i);
+        sleep(5);
     }
     
 
